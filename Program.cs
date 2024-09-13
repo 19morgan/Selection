4d6dd25d49f2f3962a28fbc164a95a9c24a72697
19mckenm <19mckenm@A13-19-24>
2024-09-13 09:55:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("What day is it? ");
+string day = Console.ReadLine();
+
+
+if (day.ToUpper().Trim() == "MONDAY") 
+{
+    Console.WriteLine("Awsome ");
+}
+else if (day.ToUpper().Trim() == "TUESDAY")
+{
+    Console.WriteLine("sad :(");
+}
+else if (day.ToUpper().Trim() == "WEDENSDAY")
+{
+    Console.WriteLine("sad :(");
+}
+else if (day.ToUpper().Trim() == "THURSDAY")
+{
+    Console.WriteLine("sad :(");
+}
+else if (day.ToUpper().Trim() == "FRIDAY")
+{
+    Console.WriteLine("sad :(");
+}
+else if (day.ToUpper().Trim() == "SATERDAY")
+{
+    Console.WriteLine("YIPEEE :)");
+}
+else if (day.ToUpper().Trim() == "SUNDAY")
+{
+    Console.WriteLine("nearly munday :(");
+}
+else
+{
+    Console.WriteLine("Thats not a day silly");
+}
+
